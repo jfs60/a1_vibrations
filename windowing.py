3a09130e5b70f868d00a49197a4b18e3e512d949
Jevon Sunandar <jevonsunandar243@gmail.com>
2023-02-09 15:10:31 +0000

    commir

--- windowing.py
+++ windowing.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pickle
 import scipy 
 from scipy.fft import fft, fftfreq, ifft
+from scipy.signal import get_window
 
 
 #setup the window function
@@ -17,26 +18,26 @@ def setting_up():
 
     initial_signal_array = np.array(initial_signal)
     initial_signal_reshaped = initial_signal_array.ravel()
-    x = np.linspace(0,30,10001)
+    x = np.linspace(0,4,10001)
     return initial_signal_reshaped
     """plt.plot(x, initial_signal_reshaped, color="blue")
     plt.show()"""
 
 def choose_range(time, initial_signal): 
-    windowing_function = np.bartlett(time+1)
+    windowing_function = np.bartlett((time*(10001/30)))
     no_of_samples = int((10001/30)*time)
     x = np.linspace(0, time, no_of_samples)
     cut_signal = (initial_signal[0:no_of_samples])
     plt.plot(x, cut_signal, color="blue")
     plt.show()
-    return cut_signal, no_of_samples
+    return cut_signal, no_of_samples, windowing_function
 
 
 
 
 def applying_fft(signal_analysed, samples): 
     sample_rate = 10001/30
-    duration = 30
+    duration = 4
     # Number of samples in normalized_tone
     N = int (sample_rate * duration)
 
@@ -44,47 +45,26 @@ def applying_fft(signal_analysed, samples):
     xf = fftfreq(N, 1 / sample_rate)
 
     plt.plot(xf, np.abs(yf))
+    plt.xlim([0, 5])   
+    plt.ylim([0,0.1])
     plt.show()
-""" N = len(fft_signal)
-    n = np.arange(N)
-    T = N/(samples/)
-    freq = n/T 
+    #not sure why they have different amplitude values 
 
-    plt.xlabel('Freq (Hz)')
-    plt.ylabel('FFT Amplitude |X(freq)|')
-
-    # Get the one-sided specturm
-    n_oneside = N//2
-    # get the one side frequency
-    f_oneside = freq[:n_oneside]
-
-    # normalize the amplitude
-    X_oneside =fft_signal[:n_oneside]/n_oneside
-
-    plt.subplot(122)
-    plt.stem(f_oneside, abs(X_oneside), 'b', \
-            markerfmt=" ", basefmt="-b")
-    plt.xlabel('Freq (Hz)')
-    plt.ylabel('Normalized FFT Amplitude |X(freq)|')
-    plt.tight_layout()
+def applying_window_function(window_function, signal_analysed, samples, time): 
+    y = window_function*signal_analysed
+    print(y)
+    x = np.linspace(0, time, samples)
+    plt.plot(x, y, color="blue")
     plt.show()
-    #sample_spacing = 1/5"""
-
-
-"""x = np.linspace(0.0, sample_spacing*samples, samples, endpoint=False)
-    xf = fftfreq(samples, sample_spacing)[:samples]
-    plt.plot(xf, 2.0/samples * np.abs(fft_signal[0:samples]))
-    plt.grid()
-    plt.show()"""
-
 
 
 
 def main (): 
     initial_signal = setting_up()
-    signal_analysed, samples = choose_range(30, initial_signal)
+    signal_analysed, samples, window_function = choose_range(4, initial_signal)
     print(signal_analysed.shape)
     applying_fft(signal_analysed, samples)
+    applying_window_function (window_function, signal_analysed, samples, 4)
 main()
 
  
